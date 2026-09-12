42c5b1f07cfa376e740354da45cd97f44c641749
MrGPT007 <mrgpt.net@gmail.com>
2026-09-12 15:18:44 -0700

    Boot Skills and Watch Me runtimes

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,4 +11,6 @@ import "./compare-read.js";
 import "./directory-extract.js";
 import "./record-write.js";
 import "./invoice-download.js";
+import "./skills-runtime.js";
+import "./watch-me-runtime.js";
 import "./background.js";
